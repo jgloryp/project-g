0ee48098bede02518df64a38d1b0617dcaca517d
James Park <jamespark@hnmcorp.kr>
2022-02-20 14:43:55 +0900

    folder 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './typeorm-config.service';
 import { UserModule } from './user/user.module';
+import { FolderModule } from './folder/folder.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { UserModule } from './user/user.module';
       useClass: TypeOrmConfigService,
     }),
     UserModule,
+    FolderModule,
   ],
   controllers: [AppController],
   providers: [AppService],
