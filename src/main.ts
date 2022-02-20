49dbb35d1ca0aee1f807deb4922f766cc83c4172
James Park <jamespark@hnmcorp.kr>
2022-02-20 14:24:59 +0900

    데이터베이스 및 API 명세 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './swagger-config.service';
@@ -6,6 +7,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   setupSwagger(app);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
