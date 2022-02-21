484dedffb1b477bc6fd2037fccc1d48c53da03a4
James Park <jamespark@hnmcorp.kr>
2022-02-21 12:28:43 +0900

    폴더를 생성할 때 포인트 1000점을 부여하는 로직 추가

--- src/folder/folder.service.ts
+++ src/folder/folder.service.ts
@@ -1,6 +1,16 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import {
+  EntityManager,
+  Repository,
+  Transaction,
+  TransactionManager,
+} from 'typeorm';
+import { Point, PointType } from '../point/entities/point.entity';
 import { User } from '../user/entities/user.entity';
 import { CreateFolderDto } from './dto/create-folder.dto';
 import { Folder } from './entities/folder.entity';
@@ -15,16 +25,39 @@ export class FolderService {
     private readonly folderRepository: Repository<Folder>,
   ) {}
 
-  async create(userId: number, folder: CreateFolderDto) {
-    const user = await this.userRepository.findOne(userId);
+  @Transaction({ isolation: 'READ COMMITTED' })
+  async create(
+    userId: number,
+    folder: CreateFolderDto,
+    @TransactionManager() manager?: EntityManager,
+  ) {
+    if (!manager) {
+      throw new InternalServerErrorException(
+        '트랜잭션 생성 중 오류가 발생 하였습니다.',
+      );
+    }
+
+    const user = await manager.findOne(User, userId);
     if (!user) {
       throw new NotFoundException('해당 유저를 찾을 수 없습니다.');
     }
 
-    const newFolder = this.folderRepository.create(folder);
+    // 새 폴더를 생성한다
+    const newFolder = manager.create(Folder, folder);
     newFolder.user = user;
+    await manager.save(Folder, newFolder);
 
-    return this.folderRepository.save(newFolder);
+    // 폴더 생성에 따른 포인트 1000점을 생성한다
+    const newPoint = manager.create(Point, {
+      type: PointType.Earned,
+      point: 1000,
+      description: `${newFolder.name} 폴더 생성`,
+      user: user,
+      folder: folder,
+    });
+    await manager.save(Point, newPoint);
+
+    return 'This action adds a new folder';
   }
 
   async findAll(userId: number) {
