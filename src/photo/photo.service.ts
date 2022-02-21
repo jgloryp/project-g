3aeba91cb7ecb7717c2625d700af5d5d48c57c57
James Park <jamespark@hnmcorp.kr>
2022-02-21 12:30:07 +0900

    사진을 업로드할 때 100포인트가 소모되며 현재 포인트가 남아 있는지 확인하도록 로직 추가

--- src/photo/photo.service.ts
+++ src/photo/photo.service.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Injectable,
   InternalServerErrorException,
   NotFoundException,
@@ -11,6 +12,7 @@ import {
   TransactionManager,
 } from 'typeorm';
 import { Folder } from '../folder/entities/folder.entity';
+import { Point, PointType } from '../point/entities/point.entity';
 import { User } from '../user/entities/user.entity';
 import { CreatePhotoDto } from './dto/create-photo.dto';
 import { CreateTagDto } from './dto/create-tag.dto';
@@ -38,7 +40,7 @@ export class PhotoService {
   // READ COMMITTED: 트랜잭션내에서 커밋된 데이터만 다른 트랜잭션이 읽는 것을 허용
   // REPEATABLE READ: 트랜잭션 내에서 한 번 조회한 데이터를 반복해서 조회해도 결과는 동일
   // SERIALIZABLE: 가장 엄격한 격리 수준으로 완벽한 읽기 일관성 모드 제공
-  @Transaction({ isolation: 'SERIALIZABLE' })
+  @Transaction({ isolation: 'READ COMMITTED' })
   async create(
     userId: number,
     folderId: number,
@@ -62,22 +64,48 @@ export class PhotoService {
       throw new NotFoundException('해당 폴더를 찾을 수 없습니다.');
     }
 
+    // 현재 포인트 합계를 가져온다
+    const { currentPoint } = await manager
+      .createQueryBuilder(Point, 'point')
+      .select('SUM(point.point)', 'currentPoint')
+      .where('point.userId = :userId', { userId })
+      .getRawOne();
+
+    // 사진을 업로드 하는데 100포인트가 필요하며, 사진 갯수에 따라 포인트 필요량을 계산한다
+    const requiredPoint = photos.length * 100;
+
+    // 포인트가 충분히 있으면 다음 업로드 로직으로 넘어간다
+    if (+currentPoint < requiredPoint) {
+      throw new BadRequestException(
+        `사진을 업로드 하기 위한 포인트가 부족합니다. (보유:${currentPoint}, 필요:${requiredPoint})`,
+      );
+    }
+
     // n개의 사진이 업로드 되었으면 n번 순환하면서 데이터 저장
     for (const photo of photos) {
+      // 사진 정보를 저장한다.
       const newPhoto = new Photo();
       newPhoto.name = photo.name;
       newPhoto.url = photo.url;
       newPhoto.user = user;
       newPhoto.folder = folder;
-
       await manager.save(Photo, newPhoto);
+
+      // 포인트 100점 사용을 저장한다 (포인트 사용의 경우는 음수로 표현)
+      const newPoint = new Point();
+      newPoint.type = PointType.Used;
+      newPoint.point = -100;
+      newPoint.description = `${newPhoto.name} 사진 업로드`;
+      newPoint.user = user;
+      newPoint.folder = folder;
+      newPoint.photo = newPhoto;
+      await manager.save(Point, newPoint);
 
       // n개의 태그가 입력 되었다면 n개의 사진에 동일한 n개의 태그 생성한다
       for (const tag of tags) {
         const newTag = new Tag();
         newTag.name = tag.name;
         newTag.photo = newPhoto;
-
         await manager.save(Tag, newTag);
       }
     }
