ff576db0aac46b299929ce046b9eb6befe7184b0
James Park <jamespark@hnmcorp.kr>
2022-02-21 12:27:49 +0900

    태그는 tags키에 comma 를 구분자로 여러개가 넘어올 수 있다

--- src/photo/photo.controller.ts
+++ src/photo/photo.controller.ts
@@ -7,14 +7,13 @@ import {
   BadRequestException,
   Get,
   Body,
-  ParseArrayPipe,
 } from '@nestjs/common';
 import { PhotoService } from './photo.service';
 import { ApiBody, ApiConsumes, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { CreatePhotoDto } from './dto/create-photo.dto';
-import { ListPhotoItem } from './dto/list-photo.dto';
 import { CreateTagDto } from './dto/create-tag.dto';
+import { ListPhotoItem } from './dto/list-photo.dto';
 
 @ApiTags('feature/photos')
 @Controller('users')
@@ -23,7 +22,7 @@ export class PhotoController {
 
   @Post(':userId/folders/:folderId/photos')
   @ApiOperation({
-    summary: `사용자의 폴더에 사진을 업로드 `,
+    summary: `사용자의 폴더에 사진들을 업로드 (사진을 업로드 하는데 100포인트가 필요하며, 사진 갯수에 따라 포인트 필요량을 계산)`,
   })
   @ApiConsumes('multipart/form-data')
   @ApiBody({
@@ -51,14 +50,17 @@ export class PhotoController {
     @Param('userId') userId: string,
     @Param('folderId') folderId: string,
     @UploadedFiles() files: Array<Express.Multer.File>,
-    @Body('tags', ParseArrayPipe) tags: Array<string>,
+    @Body('tags') tags: string,
   ) {
     console.log(files);
-
     console.log(tags);
+    console.log(tags.split(','));
 
     if (files.length === 0) {
       throw new BadRequestException('사진 파일이 첨부 되지 않았습니다');
+    }
+    if (!tags) {
+      throw new BadRequestException('태그가 입력 되지 않았습니다');
     }
 
     return this.photoService.create(
@@ -71,7 +73,7 @@ export class PhotoController {
             `https://fakepath/${file.filename}`,
           ),
       ),
-      tags.map((tag) => new CreateTagDto(tag)),
+      tags.split(',').map((tag) => new CreateTagDto(tag)),
     );
   }
 
